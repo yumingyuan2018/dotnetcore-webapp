6f2378d90e195727c35c718ff0f083ccbd3d8afb
ShehacksPurple <shehacksPurple@github.com>
2020-02-04 12:52:59 -0800

    fixed fake key in index.cshtml, hopefully

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,7 +15,9 @@ namespace dotnetcore_webapp.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            string myveryprivatekey = 'E9873D79C6D87DC0FB6A5778633389_SAMPLE_PRIVATE_KEY_DO_NOT_IMPORT_F4453213303DA61F20BD67FC233AA33262';
+            
+            string myveryprivatekey = 
+            "E9873D79C6D87DC0FB6A5778633389_SAMPLE_PRIVATE_KEY_DO_NOT_IMPORT_F4453213303DA61F20BD67FC233AA33262";
         }
 
         public void OnGet()
